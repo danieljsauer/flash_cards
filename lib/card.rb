e76c1d86bddae08f839d10c22c2c8eb15e6940bd
Daniel Sauer <danieljsauer92@gmail.com>
2023-05-09 05:57:55 -0600

    create the card object

---
+++ lib/card.rb
@@ -0,0 +1,13 @@
+class Card
+    
+    attr_reader :question, :answer, :category
+    #attr_accessor
+
+    def initialize(question, answer, category)
+        @question = question
+        @answer = answer
+        @category = category
+
+    end
+
+end 
